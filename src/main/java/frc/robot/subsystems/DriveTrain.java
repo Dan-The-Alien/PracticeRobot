25031f479311e2d09b76d03ce73ccd6d155fa621
Daniel <52894189+EcoRobotics-Daniel@users.noreply.github.com>
2019-10-31 12:25:01 -0700

    Made into Tank Drive
    I also deleted the Utitlities file, as we were not using it.

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -13,27 +13,28 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
  
 import frc.robot.RobotMap;
-
 /**
  * Add your docs here.
  */
 public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
   private VictorSPX motor_left_back = new VictorSPX(RobotMap.MOTOR_LEFT_BACK);
   private VictorSPX motor_left_front = new VictorSPX(RobotMap.MOTOR_LEFT_FRONT);
   private VictorSPX motor_right_back = new VictorSPX(RobotMap.MOTOR_RIGHT_BACK);
   private VictorSPX motor_right_front = new VictorSPX(RobotMap.MOTOR_RIGHT_FRONT);
 
-  public void setLeftMotor(double speed) {
+  public void setLeftMotors(double speed){
     motor_left_back.set(ControlMode.PercentOutput, speed);
     motor_left_front.set(ControlMode.PercentOutput, speed);
   }
 
-  public void setRightMotor(double speed) {
+  public void setRightMotors(double speed){
     motor_right_back.set(ControlMode.PercentOutput, -speed);
     motor_right_front.set(ControlMode.PercentOutput, -speed);
   }
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
